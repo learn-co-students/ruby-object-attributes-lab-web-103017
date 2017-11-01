79e45d2a6b22620d1a5975b51c9a16096a32de2c
pawlkris <paulkristapovich@gmail.com>
2017-10-31 21:59:03 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  def initialize(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+  def initialize(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+  def breed=(breed)
+    @breed = new_breed
+  end
+end
